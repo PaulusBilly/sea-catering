506f286e3792fddfb0648fe7cdd66d53a12ebaac
Billy <141384133+PaulusBilly@users.noreply.github.com>
2025-06-27 12:49:52 +0700

    replaced contact text to icons

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,17 +1,18 @@
+import Image from "next/image";
+
 export default function Contact() {
   return (
-    <section id="contact" className="py-16">
-      <div className="ml-50 container max-w-lg text center">
+    <section id="contact" className="py-16 flex justify-center">
+      <div className="container max-w-100 text-center py-2">
         <h3 className="mb-4 text-2xl font-bold">Contact Us</h3>
-        <p className="mb1">
-          <b>Manager:</b> Brian
-        </p>
-        <p className="mb-6">
-          <b>Phone:</b> <a href="tel:08123456789">081234556789</a>
-        </p>
-        <p>
-          We are happy to answer your questions or help customize your plan!
-        </p>
+        <div className="mb-4 flex gap-3">
+          <Image src="/manager.svg" alt="manager icon" width="24" height="24" />
+          <p>Brian</p>
+        </div>
+        <div className="mb-4 flex gap-3">
+          <Image src="/phone.svg" alt="phone icon" width="24" height="24" />
+          <a href="tel:08123456789">081234556789</a>
+        </div>
       </div>
     </section>
   );
